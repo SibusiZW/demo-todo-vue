493e97a4ac034840634b18c473dbece8e96c6259
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-04-17 20:23:05 +0200

    added some more code no logic yet

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div class="flex flex-col p-6 space-y-5 items-center justify-center">
+    <h1 class="text-3xl text-green-500">Demo Todo App w/ Vue.js</h1>
 
-<style scoped></style>
+    <h3>Your Tasks</h3>
+    <form>
+      <div class="flex mb-6">
+        <input type="text" placeholder="Enter new task" class="p-2.5 mr-5 rounded-md border border-green-500" required/>
+        <button type="submit" class="bg-green-600 p-2 rounded-md text-white hover:bg-green-300 transition duration-500">Add Task</button>
+      </div>
+    </form>
+  </div>
+</template>
